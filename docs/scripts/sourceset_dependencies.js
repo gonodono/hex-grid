adce3186bb54aa63dc343d91561357f570819d43
gonodono <mlmayii@yahoo.com>
2024-04-28 20:21:30 -0400

    Created docs branch.

---
+++ docs/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":view:dokkaHtmlPartial/debug":[],":view:dokkaHtmlPartial/main":[],":view:dokkaHtmlPartial/release":[],":data:dokkaHtmlPartial/debug":[],":data:dokkaHtmlPartial/main":[],":data:dokkaHtmlPartial/release":[],":compose:dokkaHtmlPartial/debug":[],":compose:dokkaHtmlPartial/main":[],":compose:dokkaHtmlPartial/release":[]}'
